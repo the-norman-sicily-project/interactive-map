c4fec3ff2b762a574be0e7341419adc3f638969a
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-09-02 14:21:33 -0400

    Update Map-Provider and Attribution Add popup with name

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -8,19 +8,26 @@ import MarkerClusterGroup from 'react-leaflet-markercluster';
 class InteractiveMap extends Component {
   constructor(props) {
     super(props);
+    this.onEachFeature = this.onEachFeature.bind(this);
     this.state = {
       center: [37.73, 14.2],
       zoom: 8,
       maxZoom: 18
     };
   }
+  onEachFeature = (feature, layer) => {
+    layer.bindPopup(feature.properties.english_place_name);
+  };
   render() {
     return (
       <div>
         <Map center={this.state.center} zoom={this.state.zoom} maxZoom={this.state.maxZoom}>
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <TileLayer
+            url="http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg"
+            attribution="Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>, under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>. Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>, under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>."
+          />
           <MarkerClusterGroup>
-            <GeoJSON data={geojson} />
+            <GeoJSON data={geojson} onEachFeature={this.onEachFeature} />
           </MarkerClusterGroup>
         </Map>
       </div>
