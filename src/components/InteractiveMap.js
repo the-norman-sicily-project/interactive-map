a82f82b0bc03b8d50701101da9ff8a42320a4494
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-09-05 13:53:30 -0400

    Move static map variables to MapConfig.js

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import { geojson } from '../data/geojson';
+import {centerPoint,tileURL,initialZoom,maxZoom,mapAttribution} from '../containers/MapConfig';
 import '../containers/App.css';
 import '../data/geojson';
-import { geojson } from '../data/geojson';
-import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 class InteractiveMap extends Component {
   constructor(props) {
     super(props);
     this.onEachFeature = this.onEachFeature.bind(this);
     this.state = {
-      center: [37.73, 14.2],
-      zoom: 8,
-      maxZoom: 18,
     };
   }
   onEachFeature = (feature, layer) => {
@@ -22,13 +20,13 @@ class InteractiveMap extends Component {
     return (
       <div>
         <Map
-          center={this.state.center}
-          zoom={this.state.zoom}
-          maxZoom={this.state.maxZoom}
+          center={centerPoint}
+          zoom={initialZoom}
+          maxZoom={maxZoom}
         >
           <TileLayer
-            url="http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg"
-            attribution="Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>, under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>. Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>, under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>."
+            url={tileURL}
+            attribution={mapAttribution}
           />
           <MarkerClusterGroup>
             <GeoJSON data={geojson} onEachFeature={this.onEachFeature} />
---
+++ src/containers/MapConfig.js
@@ -0,0 +1,5 @@
+export const centerPoint = [37.73, 14.2]
+export const initialZoom = 8
+export const maxZoom = 18
+export const tileURL="http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg"
+export const mapAttribution="Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>, under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>. Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>, under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>."
