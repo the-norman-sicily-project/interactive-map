7929bba0f0ab41f769038b137d1a96ad0dfc3a9c
Ryan Marshall <RyanMarshall5765@gmail.com>
2018-09-05 16:17:39 -0400

    Move static map variables to MapConfig.js (#12)
    * Move static map variables to MapConfig.js
    
    * Remove unneeded state
    
    * Change variables to an object
    
    * Change name from mapConfig/Options to Config

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import { geojson } from '../data/geojson';
+import { Config } from '../containers/Config';
 import '../containers/App.css';
 import '../data/geojson';
-import { geojson } from '../data/geojson';
-import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 class InteractiveMap extends Component {
   constructor(props) {
     super(props);
     this.onEachFeature = this.onEachFeature.bind(this);
-    this.state = {
-      center: [37.73, 14.2],
-      zoom: 8,
-      maxZoom: 18,
-    };
   }
   onEachFeature = (feature, layer) => {
     layer.bindPopup(feature.properties.english_place_name);
@@ -22,14 +18,11 @@ class InteractiveMap extends Component {
     return (
       <div>
         <Map
-          center={this.state.center}
-          zoom={this.state.zoom}
-          maxZoom={this.state.maxZoom}
+          center={Config.centerPoint}
+          zoom={Config.initialZoom}
+          maxZoom={Config.maxZoom}
         >
-          <TileLayer
-            url="http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg"
-            attribution="Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>, under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>. Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>, under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>."
-          />
+          <TileLayer url={Config.tileURL} attribution={Config.mapAttribution} />
           <MarkerClusterGroup>
             <GeoJSON data={geojson} onEachFeature={this.onEachFeature} />
           </MarkerClusterGroup>
---
+++ src/containers/Config.js
@@ -0,0 +1,8 @@
+export const Config = {
+  centerPoint: [37.73, 14.2],
+  initialZoom: 8,
+  maxZoom: 18,
+  tileURL: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+  mapAttribution:
+    'Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>, under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>. Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>, under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>.',
+};
