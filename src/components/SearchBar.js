7a831c8bb0783f6501925ed7aa0657e42f3e1568
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-09-21 11:16:34 -0400

    Move css import from SearchBar.js to index.html

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,6 @@ import { withLeaflet } from 'react-leaflet';
 import { GeoSearchControl } from 'leaflet-geosearch';
 import { MapControl } from 'react-leaflet';
 import SearchProvider from '../containers/SearchProvider';
-import 'leaflet-geosearch/assets/css/leaflet.css';
 
 class Search extends MapControl {
   createLeafletElement() {
