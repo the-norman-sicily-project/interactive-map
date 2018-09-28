2050b79dc2bd0811f3a15f9836bf9e17676e2a8a
Ryan Marshall <RyanMarshall5765@gmail.com>
2018-09-28 11:21:00 -0400

    Update map tile  (#16)
    * Add secret ApiKey.js to ignore
    * Add mapbox API key
    * Update tileURL and attribution
    * Add ApiKey sample / instructions
    * Change URL to nsp mapbox account
    * Change SearchBar import
    * Move search provider out of static

--- src/containers/Config.js
+++ src/containers/Config.js
@@ -2,7 +2,8 @@ export const Config = {
   centerPoint: [37.73, 14.2],
   initialZoom: 8,
   maxZoom: 18,
-  tileURL: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+  tileURL:
+    'https://api.mapbox.com/styles/v1/norman-sicily-project/cjmm4r23s7n2b2rs0tq0a9rw0/tiles/256/{z}/{x}/{y}@2x?access_token=',
   mapAttribution:
-    'Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>, under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>. Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>, under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>.',
+    '<a href=&quot;https://www.mapbox.com/about/maps/&quot;>© Mapbox</a> <a href=&quot;http://www.openstreetmap.org/copyright&quot;>© OpenStreetMap</a>',
 };
