87740a48a55dd535365cc300169e38ac87763f70
Ryan Marshall <ryan@Ryans-MBP.fios-router.home>
2018-09-29 00:13:55 -0400

    Convert JSX to plain JS

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
 registerServiceWorker();
