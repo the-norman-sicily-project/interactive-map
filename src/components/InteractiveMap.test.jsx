e24794f16ddf0750fea4142d2f24d7c64d52ad31
Joe Hayes <joephayes@gmail.com>
2018-10-03 20:52:00 -0400

    Basic Rendering Tests (#28)
    * Add basic smoke tests for all components
    * Add tests for rendered children

---
+++ src/components/InteractiveMap.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import InteractiveMap from './InteractiveMap';
+
+it('renders without crashing', () => {
+  shallow(<InteractiveMap />);
+});
+
+it('renders a map', () => {
+  const wrapper = shallow(<InteractiveMap />);
+  expect(wrapper.find('Map')).toBeDefined();
+});
+
+it('renders a tile layer', () => {
+  const wrapper = shallow(<InteractiveMap />);
+  expect(wrapper.find('TileLayer')).toBeDefined();
+});
+
+it('renders a search bar', () => {
+  const wrapper = shallow(<InteractiveMap />);
+  expect(wrapper.find('SearchBar')).toBeDefined();
+});
+
+it('renders a marker cluster group', () => {
+  const wrapper = shallow(<InteractiveMap />);
+  expect(wrapper.find('MarkerClusterGroup')).toBeDefined();
+});
