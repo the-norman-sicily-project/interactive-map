c6feef5d70710e0e1e4fd914feb806b7f8a1062c
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-10-17 15:23:41 -0400

    Add Icon component to return SVG

---
+++ src/components/icon.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({ color, icon, size }) => {
+  const styles = {
+    svg: {
+      display: 'inline-block',
+      verticalAlign: 'middle',
+    },
+    path: {
+      fill: color,
+    },
+  };
+
+  return (
+    <svg
+      style={styles.svg}
+      width={`${size}px`}
+      height={`${size}px`}
+      viewBox="0 0 1024 1024"
+    >
+      <path style={styles.path} d={icon} />
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  size: 55,
+  color: '#000000',
+};
+
+export default Icon;
