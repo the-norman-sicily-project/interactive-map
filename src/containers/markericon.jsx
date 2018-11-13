ca4992e93c4118f17147719a8037516222ca3f3f
Ryan Marshall <RyanMarshall5765@gmail.com>
2018-11-13 12:09:32 -0500

    Custom Markers and filename changes (#29)
    * Update constants to all UPPERCASE
    * Add custom markers 
    * Add svg icons to an object
    * Add Icon component to return SVG
    * Add color specific icons based upon order
    * Move scale, icon, and translate to placeTypeLookup

---
+++ src/containers/markericon.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import L from 'leaflet';
+import Icon from '../components/icon';
+import ICONS from './icons';
+
+// Using L because a component returns React.createElement() instead of L.geoJSON(..., { pointToLayer }
+// Reference: https://github.com/PaulLeCam/react-leaflet/issues/234
+
+const orderLookup = order => {
+  switch (order) {
+    case 'Basilian':
+      return { color: 'blue' };
+
+    case 'Augustinian Canons':
+      return { color: 'red' };
+
+    case 'Knights of the Hospital of Saint John of Jerusalem':
+      return { color: 'green' };
+
+    case 'Benedictine':
+      return { color: 'yellow' };
+
+    case 'Cistercian':
+      return { color: 'purple' };
+
+    case 'Cluniac':
+      return { color: 'orange' };
+
+    case 'Knights Templar':
+      return { color: 'brown' };
+
+    default:
+      return {};
+  }
+};
+const placeTypeLookup = placeType => {
+  switch (placeType) {
+    case 'monastery':
+      return { icon: ICONS.MONASTERY, scale: '1,-1', translate: '-6,-41' };
+    default: {
+      return {};
+    }
+  }
+};
+const setMarker = (feature, latlng) => {
+  const setProps = Object.assign(
+    {},
+    Icon.defaultProps,
+    placeTypeLookup(feature.properties.place_type),
+    orderLookup(feature.properties.order)
+  );
+  return L.marker(latlng, {
+    icon: L.divIcon({
+      html: ReactDOMServer.renderToString(
+        <Icon
+          icon={setProps.icon}
+          color={setProps.color}
+          translate={setProps.translate}
+          scale={setProps.scale}
+          size={setProps.size}
+        />
+      ),
+    }),
+  });
+};
+
+export default setMarker;
