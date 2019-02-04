878daac430d6a454fa611dd15df32b9cb52bcb76
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2019-02-04 17:24:24 -0500

    Add Legend component

--- src/components/legend.jsx
+++ src/components/legend.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import { Pane } from 'react-leaflet';
+import { getListOfOrders, orderLookup } from '../containers/utils';
+import geojson from '../data/geojson';
+
+const orderList = getListOfOrders(geojson);
+const makeOrderList = orderList.map(order => (
+  <li key={order}>
+    <span
+      style={{ background: Object.values(orderLookup(order)).toString() }}
+    />
+    {order}
+  </li>
+));
 
 const Legend = () => (
   <Pane>
     <div className="my-legend">
       <div className="legend-title">Map Legend</div>
       <div className="legend-scale">
-        <ul className="legend-labels">
-          <li>
-            <span style={{ backgroundColor: 'red' }} />
-            Augustinian Canons
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'blue' }} />
-            Basilian
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'yellow' }} />
-            Benedictine
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'purple' }} />
-            Cistercian
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'orange' }} />
-            Cluniac
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'brown' }} />
-            Knights Templar
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'green' }} />
-            Knights of the Hospital of Saint John of Jerusalem
-          </li>
-          <li>
-            <span style={{ backgroundColor: 'black' }} />
-            Unknown
-          </li>
-        </ul>
+        <ul className="legend-labels">{makeOrderList}</ul>
       </div>
     </div>
   </Pane>
 );
+
 export default Legend;
