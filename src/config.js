67e98dd5728a24ce67059917aa3834c8f69094d0
Joe Hayes <joephayes@gmail.com>
2021-02-21 13:00:58 -0500

    prefix REACT_APP_ on config env vars

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const CONFIG = {
   maxZoom: 18,
   mapAttribution:
     '<a href=&quot;https://www.mapbox.com/about/maps/&quot;>© Mapbox</a> <a href=&quot;http://www.openstreetmap.org/copyright&quot;>© OpenStreetMap</a>',
-  apiHost: process.env.API_SERVER_HOST || 'localhost',
-  apiPort: process.env.API_SERVER_PORT || '4000',
+  apiHost: process.env.REACT_APP_API_SERVER_HOST || 'localhost',
+  apiPort: process.env.REACT_APP_API_SERVER_PORT || '4000',
   media: {
     catalogUrl: 'http://media.normansicily.org/data/catalog.js',
   },
