dfaedeffb9f23d10819021d616d22c3408310ed6
Joe Hayes <joephayes@gmail.com>
2024-05-03 20:54:58 -0400

    http -> https

--- src/providers/search.js
+++ src/providers/search.js
@@ -4,7 +4,7 @@ import config from '../config';
 
 class SearchProvider extends JsonProvider {
   endpoint({ query, type }) {
-    return this.getUrl(`http://${config.apiHost}:${config.apiPort}/names`, {
+    return this.getUrl(`https://${config.apiHost}:${config.apiPort}/names`, {
       q: query,
       f: 'json',
     });
