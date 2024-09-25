a7e8de2ad1b23b05395f8219ad54891a061497e1
Joe Hayes <joephayes@gmail.com>
2024-09-25 14:32:16 -0400

    Require at least 3 characters for search

--- src/providers/search.js
+++ src/providers/search.js
@@ -14,31 +14,38 @@ class SearchProvider extends JsonProvider {
     if ('data' in query) {
       return Promise.resolve([query.data]);
     }
+    const { query: q } = query;
+    if (q && q.length < 3) {
+      return Promise.resolve([]);
+    }
     return super.search(query);
   };
 
-  parse({ data }) {
-    return data.reduce((accumulator, value) => {
-      const { rdfs_label, wgs_long, wgs_lat } = value;
+  parse = ({ data }) => {
+    if (data && Array.isArray(data)) {
+      return data.reduce((accumulator, value) => {
+        const { rdfs_label, wgs_long, wgs_lat } = value;
 
-      if (Array.isArray(rdfs_label)) {
-        for (const label of rdfs_label) {
+        if (Array.isArray(rdfs_label)) {
+          for (const label of rdfs_label) {
+            accumulator.push({
+              x: wgs_long,
+              y: wgs_lat,
+              label,
+            });
+          }
+        } else {
           accumulator.push({
             x: wgs_long,
             y: wgs_lat,
-            label,
+            label: rdfs_label,
           });
         }
-      } else {
-        accumulator.push({
-          x: wgs_long,
-          y: wgs_lat,
-          label: rdfs_label,
-        });
-      }
-      return accumulator;
-    }, []);
-  }
+        return accumulator;
+      }, []);
+    }
+    return [];
+  };
 }
 
 export default SearchProvider;
